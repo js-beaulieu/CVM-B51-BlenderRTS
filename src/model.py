0cbe885114bbdd4d7225b8505e8b64b7494ceb01
Nihilezah <Nihilezah@gmail.com>
2015-10-19 18:35:16 -0400

    basic
    basic

---
+++ src/model.py
@@ -0,0 +1,28 @@
+import random
+import math
+
+from unit import *
+
+class Model():
+	def __init__(self, map):
+		self.mapNb = mapNb
+		self.units = []
+		self.idNb = 0
+
+
+	def update(self):
+		pass
+
+	def createUnit(self):
+		new_unit = Unit(idNb)
+		self.units.append(new_unit)
+		self.idNb += 1
+
+	def move(self):
+		pass
+
+	def selectTargets():
+		pass
+
+
+
---
+++ src/unit.py
@@ -0,0 +1,9 @@
+class Unit():
+	def __init__(self, idNb):
+
+		self.idNb = idNb #why yellow?
+		#self.pos = pos
+		#self.owner
+		#self.dps etc...
+
+
